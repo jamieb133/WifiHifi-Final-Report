03910f500261b4a0884c7d76ca4c68e90d1e682d
JamieB133 <2127147b@student.gla.ac.uk>
2019-05-17 17:03:41 +0100

    THD FFTs

--- data/plots.m
+++ data/plots.m
@@ -1,5 +1,7 @@
-Array=csvread('./csvs/test_001_1.csv');
+Array=csvread('./csvs/ti001.csv');
 %Array=csvread('test.csv');
 col1 = Array(:, 1);
 col2 = Array(:, 2);
 plot(col1, col2)
+xlabel('Frequency') 
+ylabel('Gain(dB)') 
